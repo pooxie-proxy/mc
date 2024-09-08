09c51b4b7c0f43ce09bb48cdd4735a947a36bdf7
Pooxie Proxy Official <149209837+pooxie-proxy@users.noreply.github.com>
2024-09-08 10:10:58 -0600

    Update mineflayer.js

--- src/scripts/mineflayer.js
+++ src/scripts/mineflayer.js
@@ -1,7 +1,14 @@
 process.versions.node = '14.0.0'
 global.Buffer = global.Buffer || require('buffer').Buffer
-
+const mineflayer = require('mineflayer')
+const bot = mineflayer.createBot({
+  host: proxyHost,
+  port: proxyPort,
+  username: game.nick,
+  if game.key != null {
+    password: game.key
+  }
+})
 export default function (protocol, proxyHost, proxyPort, botConfig) {
-  require('net').setProxy({ hostname: proxyHost, port: proxyPort, protocol })
-  return require('mineflayer').createBot(botConfig)
+  bot()
 }
